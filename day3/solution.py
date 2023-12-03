ed6c421fbc3e4c195e1e9de50c49bc3766b96256
Leonardo Dos Santos Maia <ledos-sa@c1r15s3.42porto.com>
2023-12-03 18:53:25 +0000

    day 7

--- day3/solution.py
+++ day3/solution.py
@@ -1,6 +1,6 @@
 import numpy as np
-file = "data"
 file = "teste"
+file = "data"
 
 
 data = []
@@ -9,8 +9,6 @@ with open(file) as f:
   data.insert(0, list("."*len(data[0])))
   data.insert(len(data), list("."*len(data[0])))
   data = np.array(data)
-
-
 
 # print(data)
 def part1():
@@ -35,31 +33,47 @@ def part1():
         ret += int(check(start, end, linenumber))
 
   print(ret)
-part1()
+# part1()
 
 
 def part2():
   ret = 0
+
+  dataaux =[]
+  for x,  line in enumerate(data):
+    for j, c in enumerate(line):
+      if c.isdigit()== False and c != ".":
+        data[x][j]= "*"
+  print(data)
   def check(start, end, line):
-    top = list(map(lambda x: -1 if x.isdigit() or x=="."else x, data[line-1][start-1:end+2]))
-    bottom = list(map(lambda x: -1 if x.isdigit() or x=="." else x,data[line+1][start-1:end+2]))
-    right = list(map(lambda x: -1 if x.isdigit() or x=="." else x,data[:,end+1][line:line +1]))
-    left = list(map(lambda x: -1 if x.isdigit() or x=="." else x,data[:,start-1][line:line +1]))
+    top = list(map(lambda x: 1 if x== "*" else 0, data[line-1][start-1:end+2]))
+    bottom = list(map(lambda x:1 if  x=="*" else 0,data[line+1][start-1:end+2]))
+    right = list(map(lambda x:1 if  x=="*" else 0,data[:,end+1][line:line +1]))
+    left = list(map(lambda x:1 if x =="*" else 0,data[:,start-1][line:line +1]))
 
-    top = [i for i, x in enumerate(top) if x != -1]
-    bottom = [i for i, x in enumerate(bottom) if x != -1]
-    right = [i for i, x in enumerate(right) if x != -1]
-    left = [i for i, x in enumerate(left) if x != -1]
-    index = 0
-    if len(top) != 0:
-      index = [line-1, top[0]]
-    elif len(bottom) != 0:
-      index = [line+1, bottom[0]]
-    elif len(right) != 0:
-      index = [end+1, 
+    # top = [c * (x + start-1) for x, c in enumerate(top) if c == 1]
+    top = top.index(1) if 1 in top else -1
+    bottom = bottom.index(1) if 1 in bottom else -1
+    left = left.index(1) if 1 in left else -1
+    right = right.index(1) if 1 in right else -1
 
-    
-    return int("".join(data[line][start:end+1])), () if len(top+bottom+right+left) != 0 else 0
+    index =(-1, -1)
+    if (top) != -1:
+      index= (top+start-1, line-1)
+    elif (bottom) != -1:
+      index= (bottom+start-1, line+1)
+    elif (left) != -1:
+      index= (left,line)
+    elif (right) != -1:
+      index= (right, line)
+    else:
+      index=(-1, -1)
+
+    print(index)
+
+    if index != (-1, -1):
+      return int("".join(data[line][start:end+1])), index
+    return 0, [-1, -1]
 
   for linenumber, line in enumerate(data):
     number = False
@@ -71,11 +85,16 @@ def part2():
       elif c.isdigit() ==False and number:
         end = i-1
         number = False
-        ret += int(check(start, end, linenumber))
+        value , x= check(start, end, linenumber)
+        # print(value, x)
+        dataaux.append([value, x])
 
+  print(dataaux)
+  for i, x in enumerate(dataaux):
+    for j in range(i+1, len(dataaux)):
+      if x[0] != 0 and dataaux[j][0] != 0 and  x[1] == dataaux[j][1]:
+        ret += x[0] * dataaux[j][0]
+        break
   print(ret)
     
 part2()
-
-
-
